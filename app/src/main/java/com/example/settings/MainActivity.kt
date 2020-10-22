0c56029c9b3ca53935ee165ca832d6c60f84b76e
Brad <bveenvliet@gmail.com>
2020-10-22 14:39:39 -0400

    initial version

---
+++ app/src/main/java/com/example/settings/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.settings
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.settings, SettingsFragment())
+                    .commit()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
